c5c9f18306697e884014328693917eea70319f14
Daiki Okayama <lawliet.d.k.m@gmail.com>
2024-10-17 14:09:08 +0900

    Implement sieve

--- main.go
+++ main.go
@@ -11,3 +11,24 @@ func isPrime(n int) bool {
 	}
 	return true
 }
+
+func sieve(n int) []int {
+	isPrime := make([]bool, n+1)
+	for i := 2; i <= n; i++ {
+		isPrime[i] = true
+	}
+	for i := 2; i*i <= n; i++ {
+		if isPrime[i] {
+			for j := i * i; j <= n; j += i {
+				isPrime[j] = false
+			}
+		}
+	}
+	primes := []int{}
+	for i := 2; i <= n; i++ {
+		if isPrime[i] {
+			primes = append(primes, i)
+		}
+	}
+	return primes
+}
--- main_test.go
+++ main_test.go
@@ -3,7 +3,6 @@ package main
 import "testing"
 
 func TestIsPrime(t *testing.T) {
-
 	t.Run("is prime", func(t *testing.T) {
 		got := isPrime(7)
 		want := true
@@ -21,5 +20,21 @@ func TestIsPrime(t *testing.T) {
 			t.Errorf("got %t want %t", got, want)
 		}
 	})
+}
 
+func TestSieve(t *testing.T) {
+	t.Run("sieve of 10", func(t *testing.T) {
+		got := sieve(10)
+		want := []int{2, 3, 5, 7}
+
+		if len(got) != len(want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+
+		for i := range got {
+			if got[i] != want[i] {
+				t.Errorf("got %v want %v", got, want)
+			}
+		}
+	})
 }
