cb37a59f40008f7838616e8b2a9cb3018c5db28d
Daiki Okayama <lawliet.d.k.m@gmail.com>
2024-11-03 12:15:20 +0900

    Implement sub function

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,23 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn sub(left: u64, right: u64) -> u64 {
+    left - right
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn test_add() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_sub() {
+        let result = sub(2, 2);
+        assert_eq!(result, 0);
     }
 }
